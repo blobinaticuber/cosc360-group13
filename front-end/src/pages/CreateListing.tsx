bf89ad13486b544c37619dff200a5ed072ce4522
Mewli <mewlicatart@gmail.com>
2026-03-19 19:22:44 -0700

    Create listing form/page

---
+++ front-end/src/pages/CreateListing.tsx
@@ -0,0 +1,71 @@
+import Form from "../components/Form"
+
+export default function CreateListing() {
+    return(
+        <div>
+            <h1>Create a Book Listing</h1>
+
+            <Form
+                method="POST"
+                url="/api/listings"
+                contentType="application/json"
+                validator={(data) => {
+                    if (!data.title || !data.author) {
+                        alert("Title and author are required.")
+                        return false
+                    }
+
+                    if (data.year && isNaN(Number(data.year))) {
+                        alert("Year must be a number.")
+                        return false
+                    }
+
+                    return true
+                }}
+
+                onResponse={async (resPromise) => {
+                    const res = await resPromise
+
+                    if (res.ok){
+                        alert('Listing successfully created!')
+                    }
+                    else{
+                        alert('Failed to submit listing.')
+                    }
+                }}
+            >
+                <div>
+					<label>Title</label>
+					<input type="text" name="title" />
+				</div>
+
+				<div>
+					<label>Author</label>
+					<input type="text" name="author" />
+				</div>
+
+				<div>
+					<label>Year</label>
+					<input type="text" name="year" />
+				</div>
+
+				<div>
+					<label>Genre</label>
+					<input type="text" name="genre" />
+				</div>
+
+				<div>
+					<label>Description</label>
+					<textarea name="description" />
+				</div>
+
+				<div>
+					<label>Cover Image</label>
+					<input type="file" name="cover" />
+				</div>
+
+				<button type="submit">Submit</button>
+            </Form>
+        </div>
+    )
+}
