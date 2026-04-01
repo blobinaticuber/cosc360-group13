5a4befec053fbe47854b40c0e05861d1d4aa37b0
Rowan Fortier <rowan.fortier@gmail.com>
2026-04-01 00:14:19 -0700

    Start fetching books from db on home page

--- front-end/src/components/BookListing.tsx
+++ front-end/src/components/BookListing.tsx
@@ -1,14 +1,16 @@
 import "./BookListing.css"
 
-// TODO: have properties so we can update the title, description, image from database?
-// TODO: when you click on it, it should take you to a full page with more information
+type props = {
+    title: string;
+    description: string
+};
 
-function BookListing() {
+function BookListing({title, description}: props) {
     return (
         <div className="card">
             <img className="book-thumbnail" src="../../public/book_background.jpg"/>
-            <h2>Book Title</h2>
-            <p>(Description) Lorem ipsum dolar sit amet</p>
+            <h2>{title}</h2>
+            <p>{description}</p>
         </div>
     )
 }
--- front-end/src/pages/Home.tsx
+++ front-end/src/pages/Home.tsx
@@ -3,15 +3,64 @@ import Header from "../components/Header"
 import guestUser from "../util/guestUser"
 import "./Home.css"
 
+import { useState, useEffect } from "react"
+
 function Home() {
+
+	type Listing = {
+		id: string,
+		title: string,
+		description: string,
+		user: string,
+		image: string
+	}
+
+	function onQueryChange(a: string) {
+		setQuery(a)
+	}
+
+	const baseUrl = import.meta.env.VITE_SERVER_BASE_URL
+	const [books, setBooks] = useState<Listing[]>([]);
+	const [query, setQuery] = useState("");
+
+	useEffect(() => {
+		const searchTerm = query.trim() || "*";
+		fetch(`${baseUrl}/search/listing/${searchTerm}`)
+		.then(res => res.json())
+		.then(data => setBooks(data as Listing[]))
+		.catch(error => {
+			console.error("Error fetching books:", error);
+			setBooks([]);
+		});
+
+		console.log(books);
+	}, [query, baseUrl])
+
+	// const filteredBooks = books.filter(book =>
+	// 	book.title.toLowerCase().includes(query.trim().toLowerCase()) ||
+	// 	book.description.toLowerCase().includes(query.trim().toLowerCase())
+	// );
+
 	return (<>
 		<Header currentPage="/" user={guestUser()} />
-		<p>This will be the user's home page. This page should be functional even if the user isn't logged in.</p>
+		<p>Book listings</p>
+
+		<form onSubmit={e => e.preventDefault()}>
+            <input
+            placeholder="Search for title or description"
+            type="text"
+            value={query}
+            onChange={e => onQueryChange(e.target.value)}
+            />
+        </form>
+
 		<div className="book-listings-container">
-			<BookListing/>
-			<BookListing/>
+			{books.map(book => (
+				<BookListing title={book.title} description={book.description}/>))
+				// <p>There weren't any results matching your search.</p>
+			}
 		</div>
-		
+
 	</>)
 }
 
