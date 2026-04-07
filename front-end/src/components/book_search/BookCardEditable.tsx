ccdc9acf6cfc134e34520017eddf8559cdca98f9
Rowan Fortier <rowan.fortier@gmail.com>
2026-04-06 17:46:08 -0700

    new BookCardEditable component

---
+++ front-end/src/components/book_search/BookCardEditable.tsx
@@ -0,0 +1,79 @@
+import { faCalendar, faPenNib, faTags } from "@fortawesome/free-solid-svg-icons"
+import "./BookCardEditable.css"
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome"
+import type { BookDetails } from "../../server"
+import Button from "../Button"
+import { faAt, faPencil, faSave, faTrash, faUser, faEye } from "@fortawesome/free-solid-svg-icons"
+
+type BookCardProps = {
+	book: BookDetails
+	onClick?: () => void
+}
+
+function BookCard({
+	book,
+	onClick
+}: BookCardProps) {
+	return <div
+		className={"bookCard" + (onClick ? " clickable" : "")}
+		onClick={onClick}
+	>
+		<div className="imgContainer">
+			<img src={book.image} alt="book cover" />
+		</div>
+		<div className="textSection">
+			<h1>{book.title}</h1>
+			<div className="detailsContainer">
+				{book.authors && book.authors.length > 0 &&
+					<p className="authors">
+						<FontAwesomeIcon icon={faPenNib} className="icon" />
+						&nbsp;
+						{book.authors.join(", ")}
+					</p>
+				}
+				{book.publishDate &&
+					<p className="publishDate">
+						<FontAwesomeIcon icon={faCalendar} className="icon" />&nbsp;
+						{book.publishDate}
+					</p>
+				}
+				{book.categories && book.categories.length > 0 &&
+					<p className="categories">
+						<FontAwesomeIcon icon={faTags} className="icon" />
+						&nbsp;
+						{book.categories.join(", ")}
+					</p>
+				}
+			</div>
+
+            <div className="bookCardButtonContainer">
+                <Button
+                    icon={faEye}
+                    // disable={loading}
+                    className="normal"
+                    text={"Toggle Visibility"}
+                    // style={"important"}
+                    onClick={() => {
+                    }}
+                />
+                <Button
+                    icon={faTrash}
+                    // disable={loading}
+                    className="deleteAccount"
+                    text={"Delete Listing"}
+                    style={"important"}
+                    onClick={() => {
+                    }}
+                />
+            </div>
+
+
+
+		</div>
+
+
+
+    </div>
+}
+
+export default BookCard
